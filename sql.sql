487f85a986936a5019dce24131d09c0009598418
RLanzAB <bautistarenzolanz@gmail.com>
2025-05-14 13:39:10 +0800

    sql includes transaction billing table

--- sql.sql
+++ sql.sql
@@ -10,14 +10,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`user` (
   `Password` VARCHAR(255) NOT NULL,
   `Balance` DECIMAL NOT NULL DEFAULT 0,
   PRIMARY KEY (`Account_Number`),
-  UNIQUE INDEX `Account_Number_UNIQUE` (`Account_Number` ASC) VISIBLE)
-ENGINE = InnoDB;
+  UNIQUE INDEX `Account_Number_UNIQUE` (`Account_Number` ASC) VISIBLE);
 
 CREATE TABLE IF NOT EXISTS `mydb`.`payment_methods` (
   `Payment_ID` INT(1) NOT NULL,
   `Payment_Method` VARCHAR(45) NULL,
-  PRIMARY KEY (`Payment_ID`))
-ENGINE = InnoDB;
+  PRIMARY KEY (`Payment_ID`));
 
 CREATE TABLE IF NOT EXISTS `mydb`.`user_payment_junction` (
   `Payment_ID` INT(1) NOT NULL,
@@ -33,8 +31,44 @@ CREATE TABLE IF NOT EXISTS `mydb`.`user_payment_junction` (
     FOREIGN KEY (`Payment_ID`)
     REFERENCES `mydb`.`payment_methods` (`Payment_ID`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION);
+    
+    CREATE TABLE IF NOT EXISTS `mydb`.`biller` (
+  `Biller_ID` INT NOT NULL,
+  `Biller_Name` VARCHAR(45) NOT NULL,
+  `Biller_Category` VARCHAR(45) NULL,
+  PRIMARY KEY (`Biller_ID`),
+  UNIQUE INDEX `Biller_Name_UNIQUE` (`Biller_Name` ASC) VISIBLE);
+    
+    CREATE TABLE IF NOT EXISTS `mydb`.`transaction` (
+  `Transaction_ID` INT NOT NULL AUTO_INCREMENT,
+  `Account_Number` INT(6) NOT NULL,
+  `Biller_ID` INT NOT NULL,
+  `Transaction_Date` DATE NOT NULL,
+  `Transaction_Time` TIME NOT NULL,
+  `Amount` DECIMAL(10,2) NOT NULL,
+  `Payment_Method_ID` INT(1) NOT NULL,
+  `Reference_Number` VARCHAR(45) NULL,
+  PRIMARY KEY (`Transaction_ID`),
+  INDEX `transactiontouser_idx` (`Account_Number` ASC) VISIBLE,
+  INDEX `transactiontobiller_idx` (`Biller_ID` ASC) VISIBLE,
+  INDEX `transactiontopaymentmethod_idx` (`Payment_Method_ID` ASC) VISIBLE,
+  CONSTRAINT `transactiontouser`
+    FOREIGN KEY (`Account_Number`)
+    REFERENCES `mydb`.`user` (`Account_Number`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `transactiontobiller`
+    FOREIGN KEY (`Biller_ID`)
+    REFERENCES `mydb`.`biller` (`Biller_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `transactiontopaymentmethod`
+    FOREIGN KEY (`Payment_Method_ID`)
+    REFERENCES `mydb`.`payment_methods` (`Payment_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
 
 INSERT INTO `user` (`Account_Number`, `First_Name`, `Last_Name`, `Birth_Date`, `Email`, `Password`, `Balance`) VALUES
 	('123456', 'Jack', 'Master', '2012-12-12', 'jackmaster@email.com', 'jackmaster', '10000'),
